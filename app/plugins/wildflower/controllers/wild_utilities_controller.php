9717effe51d7c6ef9b4eb88bbc0c75ff694aa338
klevo <klevoo@gmail.com>
2008-11-06 21:44:54 +0100

    Utilites mass add posts

--- app/plugins/wildflower/controllers/wild_utilities_controller.php
+++ app/plugins/wildflower/controllers/wild_utilities_controller.php
@@ -2,6 +2,7 @@
 class WildUtilitiesController extends WildflowerAppController {
     
     public $components = array('Security');
+    public $uses = array('Wildflower.WildPost', 'Wildflower.WildUtility');
     
     function beforeFilter() {
         parent::beforeFilter();
@@ -17,14 +18,25 @@ class WildUtilitiesController extends WildflowerAppController {
         
     function wf_index() {
         if (!empty($this->data)) {
-            $addWhat = '_massAdd' . ucwords($this->data[$this->modelClass]['what']);
-            $howMany = intval($this->data[$this->modelClass]['how_many']);
+            $addWhat = '_massAdd' . ucwords($this->data['WildUtility']['what']);
+            $howMany = intval($this->data['WildUtility']['how_many']);
             $this->{$addWhat}($howMany);
         }
     }
     
     function _massAddPosts($howMany = 10) {
-        
+        App::import('Vendor', 'Randomizer', array('file' => 'randomizer.php'));
+        $randomizer = new Randomizer;
+        for ($i = 0; $i < $howMany; $i++) {
+            $post = array(
+                'title' => $randomizer->title(),
+                'content' => $randomizer->text(),
+                'user_id' => $this->getLoggedInUserId(),
+                'parent_id' => $randomizer->parentId($this->WildPost->id),
+            );
+            $this->WildPost->create($post);
+            $this->WildPost->save();
+        }
     }
     
 }
---
+++ vendors/randomizer.php
@@ -0,0 +1,40 @@
+<?php
+class Randomizer {
+    
+    private $_lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    private $_words;
+    private $_parentIds = array(0 => null);
+    
+    function __construct() {
+        $this->_words = explode(' ', $_lorem);
+    }
+    
+    function title() {
+        $len = mt_rand(1, 20);
+        $title = '';
+        for ($i = 0; $i < $len; $i++) {
+            $title .= $this->word() . ' ';
+        }
+        $title = trim(ucwords($title));
+        return $title;
+    }
+    
+    function text() {
+        $len = mt_rand(1, 100);
+        $text = '';
+        for ($i = 0; $i < $len; $i++) {
+            $text .= $this->_lorem;
+        }
+        return $text;
+    }
+    
+    function word() {
+        return $this->_words[array_rand($this->_words)];
+    }
+    
+    function parentId($lastId) {
+        $this->_parentIds[] = $lastId;
+        return $this->_parentIds[array_rand($this->_parentIds)];
+    }
+    
+}
