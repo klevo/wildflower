8e2b0235378fd05e9fdc831bed47dc4594a2bd3f
klevo <klevo@klevo.sk>
2008-11-30 11:27:39 +0100

    Select All/None work

--- app/plugins/wildflower/jlm/controllers/components/select_actions.js
+++ app/plugins/wildflower/jlm/controllers/components/select_actions.js
@@ -9,14 +9,14 @@ $.jlm.component('SelectActions', 'wild_posts.wf_index, wild_pages.wf_index', fun
      var handledFormEl = $('form:first');
      
      // Mark all selectedEls items
-     var selectedEls = $('input:checked');
+     var selectedEls = $('input:checked', handledFormEl);
      if (selectedEls.size() > 0) {
          selectedEls.parents('li').addClass('selected');
          selectActionsEl.show();
      }
 
      function selectionChanged() {
-         selectedEls = $('input:checked');
+         selectedEls = $('input:checked', handledFormEl);
          
          if (selectedEls.size() > 0) {
              selectActionsEl.slideDown(100);
@@ -30,15 +30,26 @@ $.jlm.component('SelectActions', 'wild_posts.wf_index, wild_pages.wf_index', fun
          return true;
      }
 
-     $('input[@type=checkbox]').click(selectionChanged);
+     $('input[@type=checkbox]', handledFormEl).click(selectionChanged);
      
-     // Bind actions
-     $('a', selectActionsEl).click(function() {
+     // Bind main actions
+     $('.select-actions > a', handledFormEl).click(function() {
          // @TODO add AJAX submit
          handledFormEl.
             append('<input type="hidden" name="data[__action]" value="' + $(this).attr('rel') + '" />').
             submit();
          return false;
      });
+     
+     // Bind select All/None
+     $('a[@href=#SelectAll]', handledFormEl).click(function() {
+         $('input:checkbox', handledFormEl).attr('checked', 'true').parents('li').addClass('selected');
+         return false;
+     });
+     
+     $('a[@href=#SelectNone]', handledFormEl).click(function() {
+          $('input:checkbox', handledFormEl).removeAttr('checked').parents('li').removeClass('selected');
+          return false;
+      });
      
 });
