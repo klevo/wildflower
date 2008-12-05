633a09d3c64acc49a4a93120b6d2071f5b037acb
klevo <klevo@klevo.sk>
2008-12-05 19:46:39 +0100

    New image insert - Close/opne

--- app/plugins/wildflower/jlm/controllers/components/tiny_mce.js
+++ app/plugins/wildflower/jlm/controllers/components/tiny_mce.js
@@ -42,17 +42,36 @@ $.jlm.addComponent('tinyMce', {
 	
 	insertImage: function(editor) {
 	    // Append img browser
-	    var browserEl = $($.jlm.element('image_browser'));
+	    var browserEl = $('#image-browser');
+	    var doBindAndLoad = false;
+	    if (browserEl.size() == 0) {
+	        doBindAndLoad = true;
+	        browserEl = $($.jlm.element('image_browser')).hide();
+	    } 
 	    $('.title-input:first').after(browserEl);
 	    
-	    // Load images
+	    if (browserEl.css('display') == 'none') {
+	        browserEl.slideDown(300);
+	    } else {
+	        // Already open, close
+	        browserEl.slideUp(200);
+	        return false;
+	    }
 	    
+	    if (!doBindAndLoad) {
+	        return false;
+	    }
+	    
+	    // Load images
 	    // @TODO: I want to do something like this:
 	    // $.jlm.url({ plugin: 'wildflower', controller: 'wild_assets', action: 'wf_insert_image' });
 	    var url = $.jlm.base + '/' + $.jlm.params.prefix + '/assets/insert_image';
 	    
 	    $.get(url, function(imagesHtml) {
+	        areImagesLoaded = true;
+	        var imagesHtmlEl = $(imagesHtml).hide();
             browserEl.prepend(imagesHtml);
+            imagesHtmlEl.fadeIn('normal');
 		});
 	    
 	    // Bind insert button
@@ -87,98 +106,12 @@ $.jlm.addComponent('tinyMce', {
 		});
 		
 		// Bind close
-        // $('#close-dialog', t.dialogEl).click(function() {
-        //  t.dialogEl.remove();
-        //  return false;
-        // });
+        $('.cancel', browserEl).click(function() {
+            browserEl.slideUp(200);
+            return false;
+        });
 	    
 	    return false;
-	    
-	    
-	    // Old stuff
-	    
-		var t = this;
-		
-		// Append dialog
-		this.dialogEl = $($.jlm.template('elements/insert_image_dialog'));
-		//debug($.jlm.template('elements/insert_image_dialog'));
-		$('body').append(this.dialogEl);
-		
-		// Size and positioning
-		var dialogWidth = Math.floor(($(window).width() / 3) * 2);
-		var dialogHeight = 500;
-        var dialogWidthHalf = Math.floor(dialogWidth / 2);
-        var dialogHeightHalf = Math.floor(dialogHeight / 2);
-        var leftPost = Math.floor($(window).width() / 2) - dialogWidthHalf; 
-        var topPost = Math.floor($(window).height() / 2) - dialogHeightHalf;
-		this.dialogEl.css({
-			left: leftPost,
-			top: topPost,
-			width: dialogWidth,
-			height: dialogHeight
-		});
-		$('#dialog-content', this.dialogEl).css('height', dialogHeight - 93);
-		
-		// Make dialog dragable
-		this.dialogEl.draggable({
-			//containment: '#wrap',
-			handle: '#dialog h3'
-		});
-		
-		// How many images will go to the browser?
-		this.limit = (dialogWidth - 16) / 128;
-		t.limit = 2 * Math.floor(t.limit);
-		
-		// Esc = close
-		$.hotkeys.add('esc', t.closeDialog);
-		
-		// Load image browser
-		var url = $.jlm.base + '/' + $.jlm.params.prefix + '/assets/insert_image/' + t.limit;
-		$.get(url, function(data) {
-			// Remove loader and append image gallery
-			t.loaderEl = $('.loader', this.dialogEl);
-			t.loaderEl.hide().before(data);
-			
-			t.bindImageSelecting();
-			t.bindPaginator();
-			
-			// Bind insert button
-			$('.submit input', t.dialogEl).click(function() {
-				var imgName = $('.selected', t.dialogEl).attr('alt');
-				
-				if (typeof(imgName) == 'undefined') {
-				    alert('Please select an image!');
-				    return false;
-				}
-				
-                // Original size (scaled)
-                var width, height;
-                if (isNaN(width = $('#ImageResizeX', t.dialogEl).val())) {
-                    width = 0;
-                }
-                if (isNaN(height = $('#ImageResizeY', t.dialogEl).val())) {
-                    height = 0;
-                }
-                var imgUrl = 'img/thumb/' + imgName + '/' + width + '/' + height;
-				
-				// Thumbnail
-				if ($('#ImageSize', t.dialogEl).val() == 'thumbnail') {
-					imgUrl = 'img/thumb/' + imgName + '/120/120/1';
-				}
-				
-				// Image HTML
-				var imgHtml = '<img alt="" src="' + imgUrl + '" />';
-				
-				editor.execCommand('mceInsertContent', 0, imgHtml);
-				return false;
-			});
-			
-			// Bind close
-			$('#close-dialog', t.dialogEl).click(function() {
-				t.dialogEl.remove();
-				return false;
-			});
-		});
 	},
 	
 	bindImageSelecting: function() {
