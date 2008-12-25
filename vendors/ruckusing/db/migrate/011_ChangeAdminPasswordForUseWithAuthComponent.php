1570c675c2a5b0573d5cc2b65de275c9cb9a7607
klevo <klevo@klevo.sk>
2008-12-25 14:53:05 +0100

    Added migration to change default admin password to Auth hashed

---
+++ vendors/ruckusing/db/migrate/011_ChangeAdminPasswordForUseWithAuthComponent.php
@@ -0,0 +1,11 @@
+<?php
+class ChangeAdminPasswordForUseWithAuthComponent extends BaseMigration {
+
+	public function up() {
+        $this->query("UPDATE users SET password = 'e569c558b6119c2948d97ff3bffd87423f75c2b1' WHERE login = 'admin'");
+	}//up()
+
+	public function down() {
+        $this->query("UPDATE users SET password = '24c05ce1409afb5dad4c5bddeb924a4bc3ea00f5' WHERE login = 'admin'");
+	}//down()
+}
