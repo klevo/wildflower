e9bd5951de98cf868f4a1fbdf567c41fd9b6de88
klevo <klevo@klevo.sk>
2008-12-27 17:27:14 +0100

    Cat reordering proof of concept

---
+++ app/plugins/wildflower/jlm/controllers/wild_categories_controller.js
@@ -0,0 +1,16 @@
+$.jlm.bind('wild_categories.wf_index', function() {
+    
+    $('.reorder-list').jTree({
+		showHelper: false,
+		hOpacity: 0.5,
+		hBg: "#FCC",
+		hColor: "#222",
+		pBorder: "1px dashed #CCC",
+		pBg: "#EEE",
+		pColor: "#222",
+		pHeight: "20px",
+		snapBack: 300,
+		childOff: 20
+	});
+    
+});
--- app/plugins/wildflower/views/wild_categories/wf_index.ctp
+++ app/plugins/wildflower/views/wild_categories/wf_index.ctp
@@ -1,1 +1,3 @@
 <h2 class="section">Reorder Blog Categories</h2>
+
+<?php echo $tree->generate($categoriesForTree, array('model' => 'WildCategory', 'class' => 'category-list reorder-list', 'element' => '../wild_categories/reorder_list_item')); ?>
