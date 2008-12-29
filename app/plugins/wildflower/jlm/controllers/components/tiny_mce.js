8daf04f85af53890bbbcf13b2267806b9c354482
klevo <klevo@klevo.sk>
2008-12-29 16:13:59 +0100

    Added fullscreen button to TinyMCE

--- app/plugins/wildflower/jlm/controllers/components/tiny_mce.js
+++ app/plugins/wildflower/jlm/controllers/components/tiny_mce.js
@@ -17,11 +17,11 @@ $.jlm.addComponent('tinyMce', {
             mode: "none",
             theme: "advanced",
             // @TODO cleanup unneeded plugins
-            plugins: "wfinsertimage,safari,style,paste,directionality,visualchars,nonbreaking,xhtmlxtras,inlinepopups",
+            plugins: "wfinsertimage,safari,style,paste,directionality,visualchars,nonbreaking,xhtmlxtras,inlinepopups,fullscreen",
             doctype: '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">',
 
             // Theme options
-            theme_advanced_buttons1: "undo,redo,|,bold,italic,strikethrough,|,formatselect,|,bullist,numlist,|,outdent,indent,blockquote,|,link,unlink,wfinsertimage,|,charmap,code",
+            theme_advanced_buttons1: "undo,redo,|,bold,italic,strikethrough,|,formatselect,|,bullist,numlist,|,outdent,indent,blockquote,|,link,unlink,wfinsertimage,|,charmap,code,fullscreen",
     		theme_advanced_buttons2: "",
     		theme_advanced_buttons3: "",
             theme_advanced_toolbar_location: "top",
