9fcd0d027eeb5c4b22579d4052841174dac973cc
klevo <klevo@klevo.sk>
2009-02-22 07:52:53 +0100

    Tests moving on

---
+++ app/plugins/wildflower/tests/fixtures/wild_revision_fixture.php
@@ -0,0 +1,8 @@
+<?php 
+class WildRevisionFixture extends CakeTestFixture {
+    public $name = 'WildRevision';
+    public $import = 'Wildflower.WildRevision';
+    // public $records = array(
+    // 
+    // );
+}
