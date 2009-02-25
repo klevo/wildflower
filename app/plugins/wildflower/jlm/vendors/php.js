c349c8517139024bbf794fbb85b4035fde510d8f
klevo <klevo@klevo.sk>
2009-02-25 07:23:31 +0100

    Fixed IE JS error

--- app/plugins/wildflower/jlm/vendors/php.js
+++ app/plugins/wildflower/jlm/vendors/php.js
@@ -16749,49 +16749,50 @@ function get_headers(url, format) {
     return headers;
 }// }}}
 
+// @Wilflower: Error in IE7
 // {{{ get_meta_tags
-function get_meta_tags(file) {
-    // Extracts all meta tag content attributes from a file and returns an array
-    // 
-    // +    discuss at: http://kevin.vanzonneveld.net/techblog/article/javascript_equivalent_for_phps_get_meta_tags/
-    // +       version: 901.1411
-    // +   original by: Brett Zamir
-    // %        note 1: This function uses XmlHttpRequest and cannot retrieve resource from different domain.
-    // %        note 1: Synchronous so may lock up browser, mainly here for study purposes.
-    // -    depends on: file_get_contents
-    // *     example 1: get_meta_tags('http://kevin.vanzonneveld.net/pj_test_supportfile_2.htm');
-    // *     returns 1: {description: 'a php manual', author: 'name', keywords: 'php documentation', 'geo_position': '49.33;-86.59'}
-
-    var fulltxt = '';
-
-    if (false) {
-        // Use this for testing instead of the line above:
-        fulltxt = '<meta name="author" content="name">'+
-        '<meta name="keywords" content="php documentation">'+
-        '<meta name="DESCRIPTION" content="a php manual">'+
-        '<meta name="geo.position" content="49.33;-86.59">'+
-        '</head>';
-    } else {
-        fulltxt = file_get_contents(file).match(/^[^]*<\/head>/i);
-    }
-    
-    var patt = /<meta[^>]*?>/gim;
-    var patt1 = /<meta\s+.*?name\s*=\s*(['"]?)(.*?)\1\s+.*?content\s*=\s*(['"]?)(.*?)\3/gim;
-    var patt2 = /<meta\s+.*?content\s*=\s*(['"?])(.*?)\1\s+.*?name\s*=\s*(['"]?)(.*?)\3/gim;
-    var txt, match, name, arr={};
-
-    while ((txt = patt.exec(fulltxt)) != null) {
-        while ((match = patt1.exec(txt)) != null) {
-            name = match[2].replace(/\W/g, '_').toLowerCase();
-            arr[name] = match[4];
-        }
-        while ((match = patt2.exec(txt)) != null) {
-            name = match[4].replace(/\W/g, '_').toLowerCase();
-            arr[name] = match[2];
-        }
-    }
-    return arr;
-}// }}}
+// function get_meta_tags(file) {
+//     // Extracts all meta tag content attributes from a file and returns an array
+//     // 
+//     // +    discuss at: http://kevin.vanzonneveld.net/techblog/article/javascript_equivalent_for_phps_get_meta_tags/
+//     // +       version: 901.1411
+//     // +   original by: Brett Zamir
+//     // %        note 1: This function uses XmlHttpRequest and cannot retrieve resource from different domain.
+//     // %        note 1: Synchronous so may lock up browser, mainly here for study purposes.
+//     // -    depends on: file_get_contents
+//     // *     example 1: get_meta_tags('http://kevin.vanzonneveld.net/pj_test_supportfile_2.htm');
+//     // *     returns 1: {description: 'a php manual', author: 'name', keywords: 'php documentation', 'geo_position': '49.33;-86.59'}
+// 
+//     var fulltxt = '';
+// 
+//     if (false) {
+//         // Use this for testing instead of the line above:
+//         fulltxt = '<meta name="author" content="name">'+
+//         '<meta name="keywords" content="php documentation">'+
+//         '<meta name="DESCRIPTION" content="a php manual">'+
+//         '<meta name="geo.position" content="49.33;-86.59">'+
+//         '</head>';
+//     } else {
+//         fulltxt = file_get_contents(file).match(/^[^]*<\/head>/i);
+//     }
+//     
+//     var patt = /<meta[^>]*?>/gim;
+//     var patt1 = /<meta\s+.*?name\s*=\s*(['"]?)(.*?)\1\s+.*?content\s*=\s*(['"]?)(.*?)\3/gim;
+//     var patt2 = /<meta\s+.*?content\s*=\s*(['"?])(.*?)\1\s+.*?name\s*=\s*(['"]?)(.*?)\3/gim;
+//     var txt, match, name, arr={};
+// 
+//     while ((txt = patt.exec(fulltxt)) != null) {
+//         while ((match = patt1.exec(txt)) != null) {
+//             name = match[2].replace(/\W/g, '_').toLowerCase();
+//             arr[name] = match[4];
+//         }
+//         while ((match = patt2.exec(txt)) != null) {
+//             name = match[4].replace(/\W/g, '_').toLowerCase();
+//             arr[name] = match[2];
+//         }
+//     }
+//     return arr;
+// }// }}}
 
 // {{{ http_build_query
 function http_build_query( formdata, numeric_prefix, arg_separator ) {
