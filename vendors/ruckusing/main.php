ccbcd79039a6a02c05c66e4502645c0f9b5d1fa6
klevo <klevo@klevo.sk>
2009-03-05 12:33:48 +0100

    some fixes

--- vendors/ruckusing/main.php
+++ vendors/ruckusing/main.php
@@ -6,7 +6,6 @@ define('RUCKUSING_BASE', dirname(__FILE__) );
 require RUCKUSING_BASE . '/lib/classes/util/class.Ruckusing_Logger.php';
 require RUCKUSING_BASE . '/config/database.inc.php';
 require RUCKUSING_BASE . '/lib/classes/class.Ruckusing_FrameworkRunner.php';
-var_dump($argv);die();
 $main = new Ruckusing_FrameworkRunner($ruckusing_db_config, $argv);
 $main->execute();
 
