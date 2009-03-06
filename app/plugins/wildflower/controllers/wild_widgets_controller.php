999cc9f12f51aee2569ac787ad8ee47eeeb2a979
klevo <klevo@klevo.sk>
2009-03-06 10:54:12 +0100

    Widgets...

--- app/plugins/wildflower/controllers/wild_widgets_controller.php
+++ app/plugins/wildflower/controllers/wild_widgets_controller.php
@@ -5,8 +5,15 @@ class WildWidgetsController extends WildflowerAppController {
         
     }
     
-    function wf_homepage_slider() {
+    function slider_config($id) {
+        if (!empty($this->data)) {
+            $config = json_encode($this->data);
+            $this->WildWidget->id = intval($id);
+            $this->WildWidget->saveField('config', $config);
+        }
         
+        $widget = $this->WildWidget->findById($id);
+        $this->data = json_decode($widget['WildWidget']['config'], true);
     }
     
 }
