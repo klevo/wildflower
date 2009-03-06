94d433431dcd442284faefc2e5f72bdad5de175c
klevo <klevo@klevo.sk>
2009-03-06 13:39:29 +0100

    Widget edit moving on...

--- app/plugins/wildflower/jlm/controllers/components/widgets.js
+++ app/plugins/wildflower/jlm/controllers/components/widgets.js
@@ -1,6 +1,12 @@
 $.jlm.addComponent('widgets', {
     
     edit: function(e) {
+        // First make sure this is a widget el
+        var jEl = $(e);
+        if (!jEl.hasClass('wf_widget')) {
+            return false;
+        }
+        
         // Hide sidebar contet
         var sidebarContent = $('#sidebar ul');
         sidebarContent.hide();
