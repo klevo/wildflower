82121d8bfda1a857281ae2f9da5ffdabdbc71d31
klevo <klevo@klevo.sk>
2009-04-19 10:44:42 +0200

    Added HTML cache helper

---
+++ wildflower/views/helpers/html_cache.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * HTML Cache CakePHP Helper
+ * Copyright (c) 2008 Matt Curry
+ * www.PseudoCoder.com
+ * http://github.com/mcurry/cakephp/tree/master/helpers/html_cache
+ * http://www.pseudocoder.com/archives/2008/09/03/cakephp-html-cache-helper/
+ *
+ * @author      mattc <matt@pseudocoder.com>
+ * @license     MIT
+ *
+ */
+
+class HtmlCacheHelper extends Helper {
+  function afterLayout() {
+    if (Configure::read('debug') > 0) {
+      return;
+    }
+
+    $view =& ClassRegistry::getObject('view');
+
+    //handle 404s
+    if ($view->name == 'CakeError') {
+      $path = $this->params['url']['url'];
+    } else {
+      $path = $this->here;
+    }
+
+    $path = implode(DS, array_filter(explode('/', $path)));
+    if($path !== '') {
+      $path = DS . ltrim($path, DS);
+    }
+    $path = WWW_ROOT . 'cache' . $path . DS . 'index.html';
+
+    $file = new File($path, true);
+    $file->write($view->output);
+  }
+}
+?>
