fa7962b61d1cdf825799e78c19270ebb3df3e348
majic3 <info@majic3.com>
2010-03-09 07:21:10 +0000

    remove inflections move wf_bootsrap to app/config/bootsrap.php - vendors/wf_core.php likely not need

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -40,24 +40,109 @@
  * $controllerPaths = array('this path to controllers', 'second full path to controllers', 'etc...');
  *
  */
+
+/**
+ * @todo find the 1.3 way set achieve this validation
+ */
+/**
+ * Not empty.
+ */
+	define('VALID_NOT_EMPTY', '/.+/');
+/**
+ * Numbers [0-9] only.
+ */
+	define('VALID_NUMBER', '/^[-+]?\\b[0-9]*\\.?[0-9]+\\b$/');
+/**
+ * A valid email address.
+ */
+	define('VALID_EMAIL', "/^[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[a-z]{2,4}|museum|travel)$/i");
+/**
+ * A valid year (1000-2999).
+ */
+	define('VALID_YEAR', '/^[12][0-9]{3}$/');
+
+/**
+ * Wrapper for application encoding respecting htmlspecialchars
+ * 
+ * @param string $string
+ * @return string Text safe to display in HTML
+ * @package wildflower
+ */
+function hsc($string) {
+	return htmlspecialchars($string, ENT_QUOTES, Configure::read('App.encoding'));
+}
+
+/**
+ * FirePHP debug
+ *
+ * @param mixed Variables to output to FireBug console
+ */
+function fb() {
+    if (Configure::read('debug') < 1) {
+        return true;
+    }
+    App::import ('Vendor', 'FirePHP', array('file' => 'FirePHP.class.php'));
+    $instance = FirePHP::getInstance(true);
+    $args = func_get_args();
+    return call_user_func_array(array($instance,'fb'),$args);
+    return true;
+}
  
-// Wildflower stuff below
+// Wildflower include core settings - bootstrap may not be needed in cake1.3
 App::import('Vendor', 'wf_bootsrap', array('file' => 'wf_bootstrap.php'));
 App::import('Vendor', 'wf_core', array('file' => 'wf_core.php'));
 
 /**
  * Wildflower Additional Class Paths
  *
- * @link http://book.cakephp.org/view/36/Additional-Class-Paths
+ * convert to App::build(array());
  */
+
 define('WILDFLOWER_DIR', APP . '..' . DS . 'wildflower' . DS);
-$viewPaths        = array(WILDFLOWER_DIR . 'views' . DS);
-$controllerPaths  = array(WILDFLOWER_DIR . 'controllers' . DS);
-$modelPaths       = array(WILDFLOWER_DIR . 'models' . DS);
-$helperPaths      = array(WILDFLOWER_DIR . 'views' . DS . 'helpers' . DS);
-$componentPaths   = array(WILDFLOWER_DIR . 'controllers' . DS . 'components' . DS);
-$behaviorPaths    = array(WILDFLOWER_DIR . 'models' . DS . 'behaviors' . DS);
-$pluginPaths      = array(WILDFLOWER_DIR . 'plugins' . DS);
-$vendorPaths      = array(WILDFLOWER_DIR . 'vendors' . DS);
-$localePaths      = array(WILDFLOWER_DIR . 'locale' . DS);
-$shellPaths       = array(WILDFLOWER_DIR . 'vendors' . DS . 'shells' . DS);
+App::build(array(
+	'plugins' => array(
+		WILDFLOWER_DIR . 'plugins' . DS,
+		APP . 'plugins' . DS
+		),
+	'models' =>  array(
+		WILDFLOWER_DIR . 'models' . DS,
+		APP . 'models' . DS
+		),
+	'views' => array(
+		WILDFLOWER_DIR . 'views' . DS,
+		APP . 'views' . DS
+		),
+	'controllers' => array(
+		WILDFLOWER_DIR . 'controllers' . DS, 
+		APP . 'controllers' . DS
+		),
+	'datasources' => array(
+		WILDFLOWER_DIR . 'models' . DS . 'datasources' . DS, 
+		APP . 'models' . DS . 'datasources' . DS
+		),
+	'behaviors' => array(
+		WILDFLOWER_DIR . 'models' . DS . 'behaviors' . DS, 
+		APP . 'models' . DS . 'behaviors' . DS
+		),
+	'components' => array(
+		WILDFLOWER_DIR . 'controllers' . DS . 'components' . DS,
+		APP . 'controllers' . DS . 'components' . DS
+		),
+	'helpers' => array(
+		WILDFLOWER_DIR . 'views' . DS . 'helpers' . DS, 
+		APP . DS . 'views' . DS . 'helpers' . DS
+		),
+	'vendors' => array(
+		WILDFLOWER_DIR . 'vendors' . DS, 
+		APP . 'vendors' . DS
+		),
+	'shells' => array(
+		WILDFLOWER_DIR . DS . 'vendors' . DS . 'shells' . DS,
+		APP . 'vendors' . DS . 'shells' . DS
+		),
+	'locales' => array(
+		WILDFLOWER_DIR . 'locales' . DS,
+		APP . 'locales' . DS
+		)
+	)
+);
--- vendors/wf_bootstrap.php
+++ vendors/wf_bootstrap.php
@@ -6,30 +6,3 @@
  *
  * @package wildflower
  */
-
-/**
- * Wrapper for application encoding respecting htmlspecialchars
- * 
- * @param string $string
- * @return string Text safe to display in HTML
- * @package wildflower
- */
-function hsc($string) {
-	return htmlspecialchars($string, ENT_QUOTES, Configure::read('App.encoding'));
-}
-
-/**
- * FirePHP debug
- *
- * @param mixed Variables to output to FireBug console
- */
-function fb() {
-    if (Configure::read('debug') < 1) {
-        return true;
-    }
-    App::import ('Vendor', 'FirePHP', array('file' => 'FirePHP.class.php'));
-    $instance = FirePHP::getInstance(true);
-    $args = func_get_args();
-    return call_user_func_array(array($instance,'fb'),$args);
-    return true;
-}
